3985b9fb91f624953c4ad3b84cf646f7c6bb60ae
Stephan Weber <sdweber422@gmail.com>
2017-06-20 20:02:03 -0700

    Adds passport and session and functions to authenticate and verify users

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const authenticate = passport.authenticate( 'local', { successRedirect: '/', failureRedirect: '/signin' } )
 const database = require('./database')
 const app = express()
+
+const checkForAuthorization = ( request, response, next ) => {
+  if ( request.isAuthenticated() ) {
+    next()
+  }
+  else {
+    response.redirect( '/' )
+  }
+}
 
 require('ejs')
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use( cookieParser() )
+app.use( session({
+  secret: 'special_session',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (request, response) => {
+app.get('/', checkForAuthorization, (request, response) => {
   database.getAlbums((error, albums) => {
     if (error) {
       response.status(500).render('error', { error: error })
@@ -26,7 +48,7 @@ app.get('/', (request, response) => {
   })
 })
 
-app.get('/albums/:albumID', (request, response) => {
+app.get('/albums/:albumID', checkForAuthorization, (request, response) => {
   const albumID = request.params.albumID
 
   database.getAlbumsByID(albumID, (error, albums) => {
@@ -39,9 +61,48 @@ app.get('/albums/:albumID', (request, response) => {
   })
 })
 
+app.get( '/signin', ( request, response ) => {
+  response.render( 'signin', { title: 'Sign In' } )
+})
+
+app.get( '/signup', ( request, response ) => {
+  response.render( 'signup', { title: 'Sign Up' } )
+})
+
+app.post( '/verifyUser', authenticate )
+
 app.use((request, response) => {
   response.status(404).render('not_found')
 })
+
+passport.serializeUser(function(user, done) {
+  console.log( 'SERIALIZE' )
+  done(null, user[0].id)
+})
+
+passport.deserializeUser(function(id, done) {
+  console.log( 'DESERIALIZE' )
+  database.getUserByID(id, function(err, user) {
+    done(err, user)
+  })
+})
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    database.getUsersByEmail(username, function (err, user) {
+      if (err) {
+        return done(err)
+      }
+      if (!user.length) {
+        return done(null, false, { message: 'Incorrect username.' })
+      }
+      if (user[0].password !== password) {
+        return done(null, false, { message: 'Incorrect password.' })
+      }
+      return done(null, user)
+    })
+  }
+))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
